691edcdca0289dc3f2ce7774e6018c5232fc7892
ashwin <ashwin@meetashwin.net>
2018-02-23 14:03:48 +0800

    remove secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://ashwintest:welcome123@ds245238.mlab.com:45238/secretslab
+#export MONGODB_CREDENTIALS=mongodb://ashwintest:welcome123@ds245238.mlab.com:45238/secretslab
 
 # finally, run the app in dev mode...
 npm run dev
